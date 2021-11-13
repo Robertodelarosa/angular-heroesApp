3247fc2ff30da0a3fb779e549051c77893d91fc1
Roberto De la Rosa Acosta <robertodelarosa117@gmail.com>
2021-11-13 16:49:26 -0600

    Creando servicio de heroes

---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Heroe } from '../interfaces/heroes.interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroesService {
+
+  private heroes: Heroe[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  getHeroes(): Observable<Heroe[]> {
+    return this.http.get<Heroe[]>('http://localhost:3000/heroes')
+  }
+}

9a939662f8bcd8d7cad43290ba80c361fdaa590d
Roberto De la Rosa Acosta <robertodelarosa117@gmail.com>
2021-11-13 18:01:19 -0600

    eliminado clase en listado.component

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -6,11 +6,6 @@ import { Heroe } from '../../interfaces/heroes.interfaces';
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styles: [
-    `
-      mat-card{
-        margin-top: 20px;
-      }
-    `
   ]
 })
 export class ListadoComponent implements OnInit {

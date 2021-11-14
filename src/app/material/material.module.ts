62deff7ece0572af387b3e63bda6c73dc0e5eee7
Roberto De la Rosa Acosta <robertodelarosa117@gmail.com>
2021-11-13 18:01:02 -0600

    Agregando modulos a material-module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,6 +6,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 
 @NgModule({
   exports: [
@@ -14,7 +17,9 @@ import { MatCardModule } from '@angular/material/card';
     MatButtonModule,
     MatIconModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatGridListModule,
+    MatProgressSpinnerModule
   ]
 })
 export class MaterialModule { }
